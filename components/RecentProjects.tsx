0dbf87c7ff1fa528df51df3d5c64f3b9ae2f94dd
SagnikGhosh18 <sagnikghosh18@gmail.com>
2025-01-25 16:05:52 +0530

    Styling changes

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,9 +13,9 @@ const RecentProjects = () => {
 
             <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
                 {projects.map(({ id, title, des, img, iconLists, link }) => (
-                    <div key={id} className='sm:h-[32rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
+                    <div key={id} className='sm:h-[40rem] h-[40rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                         <PinContainer title={link} href={link}>
-                            <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] lg:h-[30vh] mb-10'>
+                            <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[32vh] h-[30vh] lg:h-[30vh] mb-10'>
                                 <div
                                     className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                                     style={{ backgroundColor: "#13162D" }}
