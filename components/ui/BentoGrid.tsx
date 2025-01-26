5178277accb6eca06fef4a0c87cd5a61e10539cf
SagnikGhosh18 <sagnikghosh18@gmail.com>
2025-01-26 20:59:11 +0530

    Adds dynamic loading for lottie, and reverts older changes

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -4,11 +4,14 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
-import Lottie from "react-lottie";
+// import Lottie from "react-lottie";
 import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 
 export const BentoGrid = ({
     className,
